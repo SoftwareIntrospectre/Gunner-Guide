37977b0e2aa1d1f07dd95b30d15883e16540bc2a
Tony Chacon <tchacon87@gmail.com>
2016-04-11 01:21:49 -0400

    Delete TargetScript.cs

--- TargetScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TargetScript : MonoBehaviour {
-
-	//use WaitForSeconds function, maybe 3-5 seconds disable after destroyed.
-	//disable renderer and collider, wait 3 seconds, then renable it
-
-}
