8526afdac56056db3aad536c6745f27a5a48882e
Tony Chacon <tchacon87@gmail.com>
2016-06-28 07:48:50 -0400

    TimeBonusScore( );
    This is the template for the Time Bonus. Needs tweaking and testing from users. Create temporary HUD for this next.

--- ScoreManagerScript.cs
+++ ScoreManagerScript.cs
@@ -5,13 +5,31 @@ using System.Collections;
 public class ScoreManagerScript : MonoBehaviour {
 	
 	public int scoreCount;
+	public int timeBonusValue;
+
+	private int timeBonusDifference; 
+
 
 	void Start () {
 		scoreCount = 0;
+		timeBonusValue = 1000;
+	}
+
+	void Update(){
+		TimeBonusScore ();
+		Debug.Log ("Time bonus is: " + timeBonusDifference);
 	}
 		
 	public void ScoreUpdate(){
 		scoreCount += 100;
 		Debug.Log ("Score increased by ." + scoreCount);
+	}
+
+	public void TimeBonusScore(){
+		timeBonusDifference = timeBonusValue - ((int)Time.timeSinceLevelLoad * 10);
+		//create a bonus based on time ellapsed from TimerScript.
+		//time bonus = 5000 / seconds ellapsed  (round to int)
+		//Math.Round (timeBonus f / 100f, 0) * 100;
+
 	}
 }
