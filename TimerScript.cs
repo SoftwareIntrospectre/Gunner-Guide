b9900494b7297dc373e66e2862f78f1e1e28e2f5
Tony Chacon <tchacon87@gmail.com>
2016-08-23 06:49:56 -0400

    Timer Script
    This is a working version of the timer

---
+++ TimerScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour {
+	
+	private float startTime;
+	public bool timerIsStopped = false;
+	public Text timerText;
+	[HideInInspector]public ScoreManagerScript scoreManager;
+
+	void Start(){
+		scoreManager = GetComponent<ScoreManagerScript> ();
+	}
+
+	void Update(){
+
+		if (timerIsStopped)
+			return;
+
+		ActiveTimer ();
+	}
+		
+
+	public void ActiveTimer(){
+		float timeDifference = Time.timeSinceLevelLoad - startTime;
+
+		string minutes = ((int)timeDifference / 60).ToString ();
+		string seconds = (timeDifference % 60).ToString ("f2");
+
+		this.timerText.text = "Time: " + minutes + ":0" + seconds;
+
+		if (timeDifference % 60 >= 10)
+			this.timerText.text = "Time: " + minutes + ":" + seconds;
+		}
+
+
+	public void StopTimer(){ 
+		timerIsStopped = true;
+		Time.timeScale = 0;
+		timerText.color = Color.yellow;
+	}
+}
